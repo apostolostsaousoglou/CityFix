e9ff6d20ba5a842bdf8f2f2d3076bc0394afa5fa
iosifarmak <iosifarmak@gmail.com>
2026-05-26 13:58:39 +0300

    feat(map): tile URLs (CartoDB GR / ArcGIS EN), buildUrl(), useEnglish flag

--- src/com/citydamage/app/TileMapPane.java
+++ src/com/citydamage/app/TileMapPane.java
@@ -4,13 +4,24 @@ import javafx.scene.layout.Pane;
 
 public class TileMapPane extends Pane {
 
-    private static final int TILE_SIZE = 256;
+    private static final int    TILE_SIZE   = 256;
+    private static final String TILE_URL_GR = "https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png";
+    private static final String TILE_URL_EN = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}";
+    private static final String USER_AGENT  = "CityDamageReporter/1.0 (educational project)";
 
-    private double centerLat = 38.2466;
-    private double centerLon = 21.7346;
-    private int    zoom      = 13;
+    private boolean useEnglish = false;
+    private double  centerLat  = 38.2466;
+    private double  centerLon  = 21.7346;
+    private int     zoom       = 13;
 
     public TileMapPane() {
         setStyle("-fx-background-color: #ddd8cf;");
+    }
+
+    private String buildUrl(int z, int x, int y) {
+        return (useEnglish ? TILE_URL_EN : TILE_URL_GR)
+            .replace("{z}", String.valueOf(z))
+            .replace("{x}", String.valueOf(x))
+            .replace("{y}", String.valueOf(y));
     }
 }
