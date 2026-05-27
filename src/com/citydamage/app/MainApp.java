d86ccb292678532d623013554e9aadbf45ef6587
apostolostsaousoglou <paultsaousis7@gmail.com>
2026-05-27 15:31:31 +0300

    feat(core): MainApp full navigation - integrates all 5 views with callbacks

--- src/com/citydamage/app/MainApp.java
+++ src/com/citydamage/app/MainApp.java
@@ -2,7 +2,6 @@ package com.citydamage.app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -21,13 +20,45 @@ public class MainApp extends Application {
     }
 
     public void showHomePage() {
-        BorderPane placeholder = new BorderPane();
+        HomePageView homePage = new HomePageView(this::showReportPage, this::showAuthPage, this::showUsefulPage);
         if (scene == null) {
-            scene = new Scene(placeholder, 1280, 750);
+            scene = new Scene(homePage.build(), 1280, 750);
+            scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
             primaryStage.setScene(scene);
         } else {
-            scene.setRoot(placeholder);
+            scene.setRoot(homePage.build());
         }
+    }
+
+    public void showAuthPage() {
+        AuthDialog authPage = new AuthDialog(this::showHomePage);
+        scene.setRoot(authPage.build());
+    }
+
+    public void showReportPage() {
+        ReportPageView[] ref = new ReportPageView[1];
+        ref[0] = new ReportPageView(
+            this::showHomePage,
+            () -> showReportInfoPage(ref[0].getLat(), ref[0].getLng()),
+            this::showAuthPage,
+            this::showUsefulPage
+        );
+        scene.setRoot(ref[0].build());
+    }
+
+    public void showReportInfoPage(double lat, double lon) {
+        ReportInfoPageView infoPage = new ReportInfoPageView(
+            this::showReportPage,
+            () -> System.out.println("Report submitted"),
+            this::showUsefulPage,
+            lat, lon
+        );
+        scene.setRoot(infoPage.build());
+    }
+
+    public void showUsefulPage() {
+        UsefulPageView usefulPage = new UsefulPageView(this::showHomePage, this::showReportPage);
+        scene.setRoot(usefulPage.build());
     }
 
     public static void main(String[] args) {
